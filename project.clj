319c9a44f0a20d18da20e9f80c543f8998e17595
Sergey Rublev <narma.nsk@gmail.com>
2014-05-27 04:12:44 +0700

    add route-one for bidirectional routing

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [http-kit "2.1.18"]
 
                  [compojure "1.1.8"]
-                 [bidi "1.10.3"] ;; bidirectional routing
+                 [clojurewerkz/route-one "1.1.0"]
 
                  ;; [mavericklou/oauth-clj "0.1.4.1"]
                  ; [clj-oauth "1.5.1"]
